dc03fc7c224d6ddba0f1da0fa6d3daac845bba16
MarcmotenDC <superman868789@gmail.com>
2024-03-26 13:50:17 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+require "src/router.php";
+$router = new Router;
+
+// Routes
+$router->add("/", ["controller" => "home", "action" => "index"]);
+$router->add("/products", ["controller" => "products", "action" => "index"]);
+$router->add("/products/show", ["controller" => "products", "action" => "show"]);
+
+$params = $router->matchRoute($path);
+
+$controller = $params["controller"];
+$action = $params["action"];
+
+$controller = $params["controller"];
+$action = $params["action"];
+
+require "src/controllers/$controller.php";
+
+$controller_object = new $controller;
+
+$controller_object->$action();
